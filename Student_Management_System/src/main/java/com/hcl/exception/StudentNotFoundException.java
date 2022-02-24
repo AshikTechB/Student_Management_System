77c3471898e222c3325185321f749a9613d5b10b
PremPk57 <premgi2020@gmail.com>
2022-02-25 01:35:11 +0530

    initial commit

--- Student_Management_System/src/main/java/com/hcl/exception/StudentNotFoundException.java
+++ Student_Management_System/src/main/java/com/hcl/exception/StudentNotFoundException.java
@@ -1,5 +1,7 @@
 package com.hcl.exception;
 
-public class StudentNotFoundException {
-
+public class StudentNotFoundException extends Exception{
+    public StudentNotFoundException(String msg) {
+        super(msg);
+    }
 }
