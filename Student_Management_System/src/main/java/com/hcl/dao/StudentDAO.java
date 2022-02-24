1dde7d8f783e96ff5f798366e1419ed22e11c34c
PremPk57 <premgi2020@gmail.com>
2022-02-25 01:31:37 +0530

    initial commit

--- Student_Management_System/src/main/java/com/hcl/dao/StudentDAO.java
+++ Student_Management_System/src/main/java/com/hcl/dao/StudentDAO.java
@@ -1,16 +1,19 @@
 package com.hcl.dao;
 
 import java.util.List;
+
 import org.hibernate.Query;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
+
 
 public class StudentDAO {
-	public List findById(int id) {
+	@SuppressWarnings("unchecked")
+	public List<Integer> findById(int id) {
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		Query selectQuery = session.createQuery("select obj from Student obj where obj.id =:au");
 		selectQuery.setParameter("au", id);
-		List resultset = selectQuery.list();
+		List<Integer> resultset = selectQuery.list();
+		
 		session.close();
 		return resultset;
 	}
