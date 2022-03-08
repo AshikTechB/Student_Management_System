50d8f93ca46704c134042ef762829301c1201f0c
PremPk57 <premgi2020@gmail.com>
2022-03-08 09:28:00 +0530

    initial change

--- Student_Management_System/src/main/java/com/hcl/NotFoundException/StudentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.hcl.NotFoundException;
-
-public class StudentNotFoundException extends Exception{
-    public StudentNotFoundException(String msg) {
-        super(msg);
-    }
-}
---
+++ Student_Management_System/target/classes/queries.sql
@@ -0,0 +1,13 @@
+create table student(student_id number(20),primary key(student_id), student_name VARCHAR(20) NOT NULL, date_of_admission DATE);
+
+SELECT * FROM student;
+
+INSERT INTO student VALUES (001,'ASHIK','04-APR-1994');
+
+INSERT INTO student VALUES (002,'HARISH','13-MAY-1996');
+
+INSERT INTO student VALUES (003,'PREM','16-APR-1998');
+
+INSERT INTO student VALUES (004,'NAJULA','04-JUN-1999');
+
+INSERT INTO student VALUES (005,'PRAKASH','17-JUN-1999');
