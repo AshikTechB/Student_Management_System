e8f956dc7214a7b7a7360b9ff4141d2bb0378b3c
PremPk57 <96626246+PremPk57@users.noreply.github.com>
2022-03-20 17:08:47 +0530

    Delete Student_Management_System directory

--- Student_Management_System/src/main/java/com/hcl/repository/StudentLoginRepo.java
+++
@@ -1,9 +0,0 @@
-package com.hcl.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hcl.model.StudentLogin;
-
-public interface StudentLoginRepo extends JpaRepository<StudentLogin,String>{
-
-}
--- Student_Management_System/src/main/java/com/hcl/repository/StudentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.hcl.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hcl.model.Student;
-
-public interface StudentRepo extends JpaRepository<Student, Integer> {
-
-}
