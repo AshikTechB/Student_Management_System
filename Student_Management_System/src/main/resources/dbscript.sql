118e33ea96a048367ba6269baa6893a27932ae97
PremPk57 <premgi2020@gmail.com>
2022-03-22 11:01:31 +0530

    initial change

---
+++ Student_Management_System/src/main/resources/dbscript.sql
@@ -0,0 +1,23 @@
+CREATE TABLE student(
+  id NUMBER(11) ,
+   rollno  NUMBER(11),
+  name varchar(45) NOT NULL,
+  doa varchar(45) NOT NULL,
+  physics NUMBER(11) ,
+  chemistry NUMBER(11) ,
+  biology NUMBER(11) ,
+ division varchar(10),
+ result varchar(10),
+ primary key(id)
+ );
+
+ create table Staff(
+fname varchar(20),
+lname varchar(20),
+phone Number(20),
+email varchar(40),
+address varchar(40),
+userid varchar(20) NOT NULL,
+password varchar(20),
+ primary key(UserID)
+ );
