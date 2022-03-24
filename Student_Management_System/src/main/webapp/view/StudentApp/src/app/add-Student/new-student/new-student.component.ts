b9600da0356ed7bbec83c8be1d9875b0061ade9e
AshikTechB <ashiq.992@gmail.com>
2022-03-24 05:58:15 +0530

    angular code updated

--- Student_Management_System/src/main/webapp/view/StudentApp/src/app/add-Student/new-student/new-student.component.ts
+++ Student_Management_System/src/main/webapp/view/StudentApp/src/app/add-Student/new-student/new-student.component.ts
@@ -1,4 +1,8 @@
-  import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Student } from 'src/app/model/student';
+import { ActivatedRoute, Router } from '@angular/router';
+import { StudentService } from 'src/app/service/student.service';
+import { resourceLimits } from 'worker_threads';
 
 @Component({
   selector: 'app-new-student',
@@ -7,7 +11,40 @@
 })
 export class NewStudentComponent implements OnInit {
 
-  constructor() { }
+  student: Student;
+  constructor(private route: ActivatedRoute, private router: Router, private studentservice: StudentService) {
+    this.student = new Student();
+  }
+
+  onSubmit() {
+    this.findResult();
+    this.studentservice.addStudent(this.student).subscribe(data => { this.homepage() });
+  }
+  homepage() {
+    this.router.navigate(['/home']);
+  }
+  findResult() {
+    let st = new Student();
+    let physics = st.physics;
+    let biology = st.biology;
+    let chemistry = st.chemistry;
+    let score = (physics + biology + chemistry) / 3;
+    if (score >= 60) {
+      st.division = "I ";
+      st.result = "Pass";
+    }
+    else if (score >= 40 && score < 60) {
+      st.division = "II ";
+      st.result = "Pass";
+    }
+    else {
+      st.division = "III ";
+      st.result = "Fail";
+    }
+return;
+
+
+  }
 
   ngOnInit(): void {
   }
