5fac7691c241571c294392edc39c51ac953f83e0
AshikTechB <98318788+AshikTechB@users.noreply.github.com>
2022-03-28 14:44:03 +0530

    Update StudentRestApplication.java

--- Student_Management_System/src/main/java/com/hcl/StudentRestApplication.java
+++ Student_Management_System/src/main/java/com/hcl/StudentRestApplication.java
@@ -3,6 +3,12 @@ package com.hcl;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+ * Maven Based Project Using Technology
+ * 1.Spring Boot Application
+ * 2.Spring Data JPA For Database
+ * 3.Angular For All The UI
+ */
 
 @SpringBootApplication(scanBasePackages = {"com.hcl"})
 public class StudentRestApplication {
