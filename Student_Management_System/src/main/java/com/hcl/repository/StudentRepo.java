353668bf460e671c9cd84b62e2d553033e2476f2
AshikTechB <98318788+AshikTechB@users.noreply.github.com>
2022-03-28 14:41:25 +0530

    Update StudentRepo.java

--- Student_Management_System/src/main/java/com/hcl/repository/StudentRepo.java
+++ Student_Management_System/src/main/java/com/hcl/repository/StudentRepo.java
@@ -10,6 +10,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 import com.hcl.model.Student;
+/*
+ * Student repository to perform all database operations
+ */
 
 public interface StudentRepo extends JpaRepository<Student, Integer> {
 
