ae5df2ca96a6a6314694fd66e7cbe4c6279fafd3
AshikTechB <98318788+AshikTechB@users.noreply.github.com>
2022-03-28 15:49:50 +0530

    Update UserServiceImpl.java

--- Student_Management_System/src/main/java/com/hcl/service/UserServiceImpl.java
+++ Student_Management_System/src/main/java/com/hcl/service/UserServiceImpl.java
@@ -39,16 +39,6 @@ public class UserServiceImpl implements UserService {
 	}
 	
 	
-	public boolean validate_User(String username, String Password) {
-		boolean status = false;
-		ArrayList<User> users = (ArrayList<User>) userRepository.findAll();
-		for (User user : users) {
-			if ((user.getUsername().equals(username)) && user.getPassword().equals(Password))
-				return status = true;
-		}
-		return status;
-	}
-
 }
 
 ///here create a method
