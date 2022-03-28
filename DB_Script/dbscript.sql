64b0c113541e66c85b4ad72e3c6f075f6a9f9ea7
AshikTechB <ashiq.992@gmail.com>
2022-03-28 10:56:52 +0530

    DB_Script

---
+++ DB_Script/dbscript.sql
@@ -0,0 +1,38 @@
+CREATE TABLE student(
+  id NUMBER(11) ,
+  rollno  NUMBER(11),
+  name varchar(45) NOT NULL,
+  date_of_admission DATE NOT NULL,
+  physics NUMBER(11) ,
+  chemistry NUMBER(11) ,
+  biology NUMBER(11) ,
+  division varchar(10),
+  result varchar(10),
+ primary key(id)
+ );
+ 
+INSERT INTO student(id, rollno, name, date_of_admission, physics, chemistry, biology, division, result)
+VALUES('001', '101', 'Akash', '24/09/2014', '50', '65', '64', 'I', 'Pass');
+
+INSERT INTO student(id, rollno, name, date_of_admission, physics, chemistry, biology, division, result)
+VALUES('002', '102', 'Kamala', '15/05/2012', '65', '35', '25', 'II', 'Pass');
+INSERT INTO student(id, rollno, name, date_of_admission, physics, chemistry, biology, division, result)
+VALUES('003', '103', 'Varun', '20/04/2015', '35', '45', '34', 'III', 'Fail');
+    
+ create table user_detail(
+    fname varchar(20),
+    lname varchar(20),
+    phone Number(20),
+    email varchar(40),
+    address varchar(40),
+    username varchar(40) UNIQUE, 
+    userid number(20) NOT NULL,
+    password varchar(20),
+     primary key(userid)
+     );
+INSERT INTO user_detail(fname, lname, phone, email, address, username, userid, password)
+VALUES('Admin', '123', '9043846215', 'akash@gmail.com', 'Kanpur', 'Admin', '001', 'Admin@123');
+INSERT INTO user_detail(fname, lname, phone, email, address, username, userid, password)
+VALUES('Megha', 'Khan', '9567439434', 'kamkn@gmail.com', 'Bihar', 'Megha', '002', 'Megha@123');
+INSERT INTO user_detail(fname, lname, phone, email, address, username, userid, password)
+VALUES('Varun', 'Kumar', '8564138746', 'varunk@gmail.com', 'Delhi', 'Varun', '003' ,'Varun@123');
